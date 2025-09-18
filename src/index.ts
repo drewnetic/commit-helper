0520ebdff385cb7d1e2f41b77a5094b547eaea6f
Andrew Gouvêa Gonçalves de Souza <103960552+AndrewGouvea@users.noreply.github.com>
2025-09-18 18:37:50 -0300

    feat(git): add function to fetch staged files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("🚀 Commit Helper is running!")
+import { getStagedFiles } from "./infra/git";
+
+const files = getStagedFiles()
+
+if(files.length === 0) {
+  console.log("⚠️ No staged files found. Stage some files with `git add`.")
+} else {
+  console.log("📂 Staged files:")
+  files.forEach((file) => console.log(" -", file))
+}
---
+++ src/infra/git.ts
@@ -0,0 +1,21 @@
+import { execSync } from 'child_process';
+
+/**
+ * Get staged files from Git.
+ * @returns string[] List of staged file paths
+ */
+export const getStagedFiles = (): string[] => {
+  try {
+    const output = execSync("git diff --staged --name-only", {
+      encoding: "utf-8"
+    })
+
+    return output
+    .split("\n")
+    .map((file) => file.trim())
+    .filter(Boolean) // remove linhas vazias
+  } catch(error) {
+    console.log("❌ Failed to get staged files. Make sure you are in a Git repo.")
+    return []
+  }
+}
